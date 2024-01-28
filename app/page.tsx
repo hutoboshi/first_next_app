751fe5afdfa188058518ccfafb4d6f458fa82b26
taisei <hutoboshi.eng@gmail.com>
2024-01-28 13:09:49 +0900

    20240128 create Modal component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import MainHeader from "./components/header/MainHeader";
 import MainRow from "./components/mainRow/MainRow";
 import MainRow2 from "./components/mainRow/MainRow2";
+import { PopUpComponent } from "./components/popUp/popUpComponent";
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [popupFlag, setPopupFlag] = useState<boolean>(false);
 
   const testItems = []
   for(let i=0; i<20; i++){
@@ -20,6 +25,11 @@ export default function Home() {
     <MainHeader></MainHeader>
     <MainRow></MainRow>
     <MainRow2></MainRow2>
+    <div className="flex justify-center items-center">
+      {/* <button onClick={() => setPopupFlag(true)}>popup</button> */}
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => setPopupFlag(true)}>Default</button>
+    </div>
+      <PopUpComponent viewFlag={popupFlag} setViewFlag={setPopupFlag} />
     {testItems}
     </>
   );
